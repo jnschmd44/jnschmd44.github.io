192e10c714b8fd85021defa81f9f168bffc88e1d
Jan Niklas <162377368+jnschmd44@users.noreply.github.com>
2024-03-06 03:44:21 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,6 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
-  [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Leibniz University Hannover",
+    52.38592864478714, 9.71365000276521
   ]
 ];
